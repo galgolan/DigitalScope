56a0ceda0e5ca72b62bc4c9f5cd6599ac913f18c
unknown <galgo@t-galgo-9020.middleeast.corp.microsoft.com>
2015-08-26 18:24:18 +0300

    changes to builder

--- ScopeUI/ScopeUI/ScopeUI/main.c
+++ ScopeUI/ScopeUI/ScopeUI/main.c
@@ -1,8 +1,28 @@
-#include <gtk/gtk.h>
+#include <gtk-3.0\gtk\gtk.h>
 
-#include "app.h"
+#include "common.h"
+#include "screen.h"
+#include "serial.h"
 
 int main(int argc, char *argv[])
 {
-	return g_application_run(G_APPLICATION(scope_app_new()), argc, argv);
+	GtkBuilder      *builder;
+	GtkWidget       *window;
+
+	gtk_init(&argc, &argv);
+
+	builder = gtk_builder_new();
+	gtk_builder_add_from_file(builder, "ui.xml", NULL);
+	window = GTK_WIDGET(gtk_builder_get_object(builder, "window1"));
+	gtk_builder_connect_signals(builder, NULL);
+
+	g_object_unref(G_OBJECT(builder));
+
+	gtk_widget_show(window);
+
+	screen_init();
+
+	gtk_main();
+
+	return 0;
 }
